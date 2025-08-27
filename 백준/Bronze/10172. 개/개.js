42b402c95c41fcae1b736ffc0d6301228315a510
SeungCheol <153044063+kimseungcheo@users.noreply.github.com>
2025-08-27 22:11:16 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+console.log("|\\_/|");
+console.log("|q p|   /}");
+console.log("( 0 )\"\"\"\\");
+console.log("|\"^\"`    |");
+console.log("||_/=\\\\__|");
