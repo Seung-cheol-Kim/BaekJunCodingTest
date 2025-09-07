c56278de1724fd8b6745cef834ba6cfa9d787143
SeungCheol <153044063+kimseungcheo@users.noreply.github.com>
2025-09-08 00:22:08 +0900

    [Bronze V] Title: 이상한 기호, Time: 96 ms, Memory: 9340 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+console.log((A + B) * (A - B));
